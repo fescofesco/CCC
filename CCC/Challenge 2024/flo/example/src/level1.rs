a84f69f5fe6ca7cce1fdb7ff8d907f4d5d1865ab
nebulark <florian.schmiderer@posteo.net>
2024-10-25 15:30:41 +0200

    flo2

--- CCC/Challenge 2024/flo/example/src/level1.rs
+++ CCC/Challenge 2024/flo/example/src/level1.rs
@@ -1,5 +1,11 @@
 use std::{fs::{read_to_string, File}, io::{Read, Write}};
 
+struct Room
+{
+    x : u32,
+    y : u32
+}
+
 pub fn part1()
 {
     let mut file = File::open("input/level1/level1_1.in").unwrap();
@@ -7,10 +13,22 @@ pub fn part1()
     file.read_to_string(&mut filecontent).unwrap();
 
     let mut lines = filecontent.lines();
+    let room_count : i32 = lines.next().unwrap().parse().unwrap();
+    let mut rooms = Vec::new();
+    for line in lines {
+        let mut token: std::str::SplitAsciiWhitespace<'_> = line.split_ascii_whitespace();
+        rooms.push(Room{
+            x : token.next().unwrap().parse().unwrap(),
+            y : token.next().unwrap().parse().unwrap(),
+        });
+    }
+
+    assert!(rooms.len() == room_count as usize);
 
     let mut outstring = String::new();
-    for line in lines {
-        outstring.push_str(&format!("{0}\n", line));
+    for r in rooms {
+
+        outstring.push_str(&format!("{0}\n", (r.x / 3) * r.y));
     }
 
     let mut outfile = File::create("output/level1/level1_1.out").unwrap();
