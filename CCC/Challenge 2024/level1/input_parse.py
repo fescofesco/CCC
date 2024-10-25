014ce790c11d03e4699e479ed0974d08ea014445
Joachim Dunkel <joachim.dunkel@knapp.at>
2024-10-25 15:52:55 +0200

    added level 3 files

--- CCC/Challenge 2024/level1/input_parse.py
+++ CCC/Challenge 2024/level1/input_parse.py
@@ -1,4 +1,5 @@
-from optimize import max_desks_in_room
+from dataclasses import dataclass
+from typing import List
 
 
 def load_inputs(location, ending=".in"):
@@ -10,27 +11,42 @@ def load_inputs(location, ending=".in"):
     return input_files
 
 
+@dataclass
+class RoomDeskInfo:
+    x: int
+    y: int
+    desk_count: int
+    id: int
+
+
 def parse_input(input_string):
     lines = input_string.strip().split('\n')
     N = int(lines[0])  # Number of rooms
-    rooms = []
 
-    # Parse each room dimension
+    room_infos = []
     for i in range(1, N + 1):
-        x, y = map(int, lines[i].split())
-        rooms.append((x, y))
-
-    return rooms
-
+        x, y, desk_count = map(int, lines[i].split())
+        room_infos.append(RoomDeskInfo(x, y, desk_count, i))
+    return room_infos
 
 
-
-def calculate_desks(rooms):
-    desk_counts = [max_desks_in_room(x, y) for x, y in rooms]
-    return desk_counts
 
 def format_output(desk_counts):
     return '\n'.join(map(str, desk_counts))
+
+
+def provide_room_id_matrix(room_infos: List[RoomDeskInfo]) -> str:
+    room_id_matrix = ""
+
+    room_count = 0
+    for room_info in room_infos:
+        for i in range(3):
+            room_id_matrix += f"{room_info.id} "
+
+        if room_count == room_infos.x / 3:
+            room_id_matrix += "\n"
+        room_count += 1
+    return room_id_matrix
 
 if __name__ == '__main__':
     import os
@@ -43,9 +59,7 @@ if __name__ == '__main__':
         with open(f_p, "r") as file:
             input_string = file.read()
         rooms = parse_input(input_string)
-        desk_counts = calculate_desks(rooms)
-        
-        output_string = format_output(desk_counts)
+        room_result = provide_room_id_matrix(rooms)
         out_fp = output_location / (Path(f_p).stem + ".out")
         with open(out_fp, "w") as file:
-            file.write(output_string)
+            file.write(room_result)
